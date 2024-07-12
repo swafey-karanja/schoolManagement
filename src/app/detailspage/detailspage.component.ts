c6b82312c113a8d044ffe2cba1e0eb62e0bf5b4d
swafey karanja <kswafey@gmail.com>
2024-07-12 20:15:51 +0300

    Lane Boy

--- src/app/detailspage/detailspage.component.ts
+++ src/app/detailspage/detailspage.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-detailspage',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent,
+    CommonModule
+  ],
   templateUrl: './detailspage.component.html',
   styleUrl: './detailspage.component.css'
 })
 export class DetailspageComponent {
+
+  invoices = [
+    {InvoiceID: '0001', Status: 'Pending', DueDate: '2024-09-23'},
+    {InvoiceID: '0002', Status: 'Pending', DueDate: '2024-09-20'},
+    {InvoiceID: '0003', Status: 'Collected', DueDate: '2024-04-03'},
+    {InvoiceID: '0004', Status: 'Collected', DueDate: '2024-06-12'},
+    {InvoiceID: '0005', Status: 'Collected', DueDate: '2024-02-27'},
+  ]
+
+  ngOnInit () {
+    this.sortInvoices();
+  }
+
+  sortInvoices () {
+    this.invoices.sort((a, b) => {
+      if (a.Status === 'Pending' && b.Status !== 'Pending') {
+        return -1;
+      } else if (a.Status !== 'Pending' && b.Status === 'Pending') {
+        return 1;
+      } else {
+        return new Date(a.DueDate).getTime() - new Date(b.DueDate).getTime();
+      }
+    })
+  }
 
 }
